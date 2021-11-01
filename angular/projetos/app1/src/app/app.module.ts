805b6e2e499123d83692ad6acb1a5a330d3a0c4e
lucas bastos <lucas.bastos.nas@gmail.com>
2021-11-01 15:38:09 -0300

    wip-criando o componente top

--- angular/projetos/app1/src/app/app.module.ts
+++ angular/projetos/app1/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TopoComponent } from './topo/topo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector:'app-topo',
+    templateUrl:'./topo.component.html'
+})
+export class TopoComponent {
+
+}
