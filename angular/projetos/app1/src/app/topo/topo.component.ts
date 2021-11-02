ce2ae48d5a4b20ab69dd0f16ce998945d503f7bb
lucas bastos <lucas.bastos.nas@gmail.com>
2021-11-02 12:02:49 -0300

    wip-criando os componetes

--- angular/projetos/app1/src/app/topo/topo.component.ts
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -2,8 +2,10 @@ import { Component } from "@angular/core"
 
 @Component({
     selector:'app-topo',
-    templateUrl:'./topo.component.html'
+    templateUrl:'./topo.component.html',
+    styleUrls:['./topo.component.css']
+    
 })
 export class TopoComponent {
-
+    public titulo: string = 'Aprendendo Inglês'
 }
