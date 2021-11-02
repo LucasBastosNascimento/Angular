2f39e969db4020c87d392c3104b3922fb429d478
lucas bastos <lucas.bastos.nas@gmail.com>
2021-11-02 12:05:07 -0300

    wip- Alterando o componente topo

--- angular/projetos/app1/src/app/app.module.ts
+++ angular/projetos/app1/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule,
