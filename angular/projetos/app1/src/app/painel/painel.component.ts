6f345ea9b6ec61d5726d8c1cc4b820b1f8ed6f18
lucas bastos <lucas.bastos.nas@gmail.com>
2021-11-17 19:56:47 -0300

    App1(Seção11): Estabelecendo a lógica das rodadas de frases

--- angular/projetos/app1/src/app/painel/painel.component.ts
+++ angular/projetos/app1/src/app/painel/painel.component.ts
@@ -11,12 +11,23 @@ export class PainelComponent implements OnInit {
   public frases: Frase[]= FRASES
   public instrucao: string='Traduza a frase:'
   public resposta: string=''
-  constructor() {console.log(this.frases) }
+
+  public rodada: number=0
+
+  public rodadaFrase!: Frase;
+
+  constructor() {
+    this.rodadaFrase =this.frases[this.rodada]
+    console.log(this.frases) 
+  }
 
   ngOnInit(): void {
   }
 
   public atualizaResposta(resposta: Event): void{
     this.resposta=(<HTMLInputElement>resposta.target).value;
+  }
+  public verificarResposta(): void{
+    console.log('Verificar resposta: ', this.resposta)
   }
 }
